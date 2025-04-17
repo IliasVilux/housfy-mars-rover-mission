aee86ff120068f3c96fa716f8fa63a5b0934ecf2
IliasVilux <iliasotsman@gmail.com>
2025-04-17 13:48:27 +0200

    feat: :sparkles: Add "Rover" class logic

--- index.php
+++ index.php
@@ -6,10 +6,5 @@ use MarsRover\Planet;
 use MarsRover\Rover;
 
 $planet = new Planet();
-echo "Planeta creado! Tiene un tamaño de {$planet->size} \n";
-echo "Obstáculos generados:\n";
-foreach ($planet->obstacles as $index => $obstacle) {
-    echo "- Obstáculo #$index en posición ({$obstacle->x}, {$obstacle->y})\n";
-}
 $rover = new Rover();
-echo "Rover creado!";
+$rover->move();
--- src/Rover.php
+++ src/Rover.php
@@ -2,9 +2,86 @@
 
 namespace MarsRover;
 
+use MarsRover\Exceptions\OutOfBoundsException;
+use MarsRover\Exceptions\ObstacleEncounteredException;
+
 class Rover
 {
-    public function __construct()
+    public function __construct(public int $x = 0, public int $y = 0, public string $direction = "N")
     {
+        $this->direction = strtoupper($direction);
+        $validDirections = ['N', 'S', 'E', 'W'];
+
+        if (!in_array($this->direction, $validDirections)) {
+            throw new \InvalidArgumentException("La dirección {$this->direction} es inválida. (N, S, E o W)");
+        }
+    }
+
+    public function move(string $commands, Planet $planet): void
+    {
+        $validMoves = ['F', 'L', 'R'];
+
+        foreach (str_split(strtoupper($commands)) as $command)
+        {
+            if (!in_array($command, $validMoves)) {
+                throw new \InvalidArgumentException("El movimiento {$this->command} es inválido. (F, L ,R)");
+            }
+
+            switch ($command)
+            {
+                case "F":
+                    $this->moveForward($planet);
+                    break;
+                case "L":
+                    $this->turn(-1);
+                    break;
+                case "R":
+                    $this->turn(1);
+                    break;
+            }
+        }
+    }
+
+    private function moveForward(Planet $planet): void
+    {
+        $tmpX = $this->x;
+        $tmpY = $this->y;
+
+        switch ($this->direction)
+        {
+            case "N":
+                $tmpY++;
+                break;
+            case "S":
+                $tmpY--;
+                break;
+            case "E":
+                $tmpX++;
+                break;
+            case "W":
+                $tmpX--;
+                break;
+        }
+
+        if ($tmpX < 0 || $tmpX >= $planet->size || $tmpY < 0 || $tmpY >= $planet->size)
+        {
+            throw new OutOfBoundsException($tmpX, $tmpY, $this->x, $this->y);
+        }
+
+        if ($planet->isClear($tmpX, $tmpY))
+        {
+            $this->x = $tmpX;
+            $this->y = $tmpY;
+        } else {
+            throw new ObstacleEncounteredException($tmpX, $tmpY, $this->x, $this->y);
+        }
+    }
+
+    private function turn(int $rotation): void
+    {
+        $clockwiseDirections = ['N', 'E', 'S', 'W'];
+        $currentDirectionIndex = array_search($this->direction, $clockwiseDirections);
+
+        $this->direction = $clockwiseDirections[($currentDirectionIndex + $rotation + 4) % 4];
     }
 }
