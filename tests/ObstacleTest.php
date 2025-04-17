e27fc2c41e00147856b1d875e2e5b9144ddbffb6
IliasVilux <iliasotsman@gmail.com>
2025-04-17 20:22:06 +0200

    test: :white_check_mark: Add unit tests for "Obstacle" class

--- tests/ObstacleTest.php
+++ tests/ObstacleTest.php
@@ -5,4 +5,12 @@ use MarsRover\Obstacle;
 
 final class ObstacleTest extends TestCase
 {
+    public function testIsOccupiedByObstacle(): void
+    {
+        $obstacle = new Obstacle(0, 0);
+        $this->assertTrue($obstacle->isOccupied(0, 0));
+        $this->assertFalse($obstacle->isOccupied(1, 0));
+        $this->assertFalse($obstacle->isOccupied(0, 1));
+        $this->assertFalse($obstacle->isOccupied(1, 1));
+    }
 }
